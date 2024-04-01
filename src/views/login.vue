43374b3b248f3911ada688a3e3a7ed7223d38edc
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-04-01 16:33:02 +0000

    Update login form and add login functionality

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,28 +1,42 @@
 <template>
     <div>
       <h1>Login</h1>
-      <form>
+      <form @submit.prevent="login">
             <div class="form-group">
                 <label for="email">Email</label>
-                <input type="email" id="email" v-model="email" class="form-control" required>
+                <input type="email" name="email" id="email" v-model="email" class="form-control" required>
             </div>
             <div class="form-group">
                 <label for="password">Password</label>
-                <input type="password" id="password" v-model="password" class="form-control" required>
+                <input type="password" name="password" id="password" v-model="password" class="form-control" required>
             </div>
-            <button type="submit" class="btn btn-primary">Register</button>
+            <button type="submit" class="btn btn-primary">Login</button>
             <router-link :to="{path : 'register'}" class="text-dark"> go to Register</router-link>
         </form>
     </div>
   </template>
 
   <script>
-
+  import axios from 'axios';
   export default {
     data() {
       return {
         email: '',
         password: ''
+      }
+    },
+    methods: {
+      login(){
+        axios.post('http://localhost:8000/api/login',{
+          email: this.email,
+          password: this.password
+        })
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        })
       }
     }
   }
