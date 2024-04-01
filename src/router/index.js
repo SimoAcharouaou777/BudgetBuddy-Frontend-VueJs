014cb70334c585583faeed80ef80f5cac1fc64d4
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-04-01 16:19:09 +0000

    Add register and login routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import register from '../views/register.vue'
+import login from '../views/login.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +18,16 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path:'/register',
+      name:'register',
+      component:register
+    },
+    {
+      path:'/login',
+      name:'login',
+      component:login
     }
   ]
 })
