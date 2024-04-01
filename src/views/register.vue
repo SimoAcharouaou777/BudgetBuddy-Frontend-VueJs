d341e07ffee02df07c7380defcf8367562e160eb
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-04-01 16:19:17 +0000

    Add register view and form submission logic

---
+++ src/views/register.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+        <h1>Register</h1>
+        <form @submit.prevent="register">
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" id="name" name="name" v-model="name" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" id="email" v-model="email" name="email" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" v-model="password" class="form-control" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Register</button>
+        </form>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            name: '',
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        register(){
+            axios.post('http://localhost:8000/api/register',{
+                name: this.name,
+                email: this.email,
+                password: this.password
+            })
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        }
+    }
+}
+</script>
